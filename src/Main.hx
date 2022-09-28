ad3ba75b3256d88a6ae2e8b14bb40b7f52510b4b
jf <j@b.f>
2022-09-28 12:04:47 +0100

    from grig.midi repo

---
+++ src/Main.hx
@@ -0,0 +1,47 @@
+package;
+
+import grig.midi.MidiMessage;
+import grig.midi.MidiOut;
+
+import haxe.Timer;
+
+import tink.core.Future;
+
+class Main
+{
+
+    private static function mainLoop(midiOut:MidiOut)
+    {
+        var counter:Int = 0;
+        var beatTimer = new Timer(500);
+
+        beatTimer.run = function() {
+            midiOut.sendMessage(MidiMessage.ofArray(counter % 2 == 0 ? [144,54,70] : [128,54,64]));
+            if (counter == 7) beatTimer.stop();
+            counter++;
+        }
+    }
+
+    static function main()
+    {
+        trace(MidiOut.getApis());
+        var midiOut = new MidiOut(grig.midi.Api.Unspecified);
+        midiOut.getPorts().handle(function(outcome) {
+            switch outcome {
+                case Success(ports):
+                    trace(ports);
+                    midiOut.openPort(0, 'grig.midi').handle(function(midiOutcome) {
+                        switch midiOutcome {
+                            case Success(_):
+                                mainLoop(midiOut);
+                            case Failure(error):
+                                trace(error);
+                        }
+                    });
+                case Failure(error):
+                    trace(error);
+            }
+        });
+    }
+
+}
